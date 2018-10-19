d83f465b3015e34d37a8b2c378fa33beefbf5f81
Hussein0000 <husseinyehia0@gmail.com>
2018-10-19 20:45:01 +0300

    У меня есть проблема , когда я нажимаю run , мне показалось что есть ошибки в тесте И я вообще не трогал тест

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -1,6 +1,8 @@
 @file:Suppress("UNUSED_PARAMETER")
+
 package lesson1.task1
 
+import java.lang.Math.pow
 import kotlin.math.*
 
 /**
@@ -29,8 +31,9 @@ fun discriminant(a: Double, b: Double, c: Double) = sqr(b) - 4 * a * c
  *
  * Поиск одного из корней квадратного уравнения
  */
-fun quadraticEquationRoot(a: Double, b: Double, c: Double) =
-        (-b + sqrt(discriminant(a, b, c))) / (2 * a)
+fun quadraticEquationRoot(a: Double, b: Double, c: Double) {
+    (-b + sqrt(discriminant(a, b, c))) / (2 * a)
+}
 
 /**
  * Пример
@@ -58,8 +61,8 @@ fun main(args: Array<String>) {
  * Пользователь задает время в часах, минутах и секундах, например, 8:20:35.
  * Рассчитать время в секундах, прошедшее с начала суток (30035 в данном случае).
  */
-fun seconds(hours: Int, minutes: Int, seconds: Int): Int {
-    (hours*3600 + minutes*60 + seconds)
+fun seconds(hours: Int, minutes: Int, seconds: Int) {
+    (hours * 3600 + minutes * 60 + seconds)
 }
 
 /**
@@ -69,18 +72,18 @@ fun seconds(hours: Int, minutes: Int, seconds: Int): Int {
  * Определить длину того же отрезка в метрах (в данном случае 18.98).
  * 1 сажень = 3 аршина = 48 вершков, 1 вершок = 4.445 см.
  */
-fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = TODO()
-fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double {
+fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int) {
     (sagenes * 48 * 0.04445 + arshins * 16 * 0.04445 + vershoks * 0.04445)
 }
+
 /**
  * Тривиальная
  *
  * Пользователь задает угол в градусах, минутах и секундах (например, 36 градусов 14 минут 35 секунд).
  * Вывести значение того же угла в радианах (например, 0.63256).
  */
-fun angleInRadian(grad: Int, min: Int, sec: Int): Double {
-    (grad / 180.0 * PI + min / 60.0 / 180 * PI + sec / 3600.0 / 180 * PI)
+fun angleInRadian(grad: Int, min: Int, sec: Int) {
+    (grad / 180.0 * PI + (min / 60.0).div(180) * PI + sec / 3600.0 / 180 * PI)
 
 }
 
@@ -90,7 +93,7 @@ fun angleInRadian(grad: Int, min: Int, sec: Int): Double {
  * Найти длину отрезка, соединяющего точки на плоскости с координатами (x1, y1) и (x2, y2).
  * Например, расстояние между (3, 0) и (0, 4) равно 5
  */
-fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double {
+fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double) {
     (sqrt(sqr(x1 - x2) + sqr(y1 - y2)))
 
 }
@@ -101,7 +104,7 @@ fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double {
  * Пользователь задает целое число, большее 100 (например, 3801).
  * Определить третью цифру справа в этом числе (в данном случае 8).
  */
-fun thirdDigit(number: Int): Int {
+fun thirdDigit(number: Int) {
     (number / 100 % 10)
 }
 
@@ -115,8 +118,7 @@ fun thirdDigit(number: Int): Int {
 fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int {
     val x1 = 60 - minutesDepart + minutesArrive
     val hours = hoursArrive - hoursDepart - 1
-    val minutes = x1 + 60 * hours
-    return minutes
+    return x1 + 60 * hours
 }
 
 /**
@@ -126,7 +128,8 @@ fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minute
  * Сколько денег будет на счету через 3 года (с учётом сложных процентов)?
  * Например, 100 рублей под 10% годовых превратятся в 133.1 рубля
  */
-fun accountInThreeYears(initial: Int, percent: Int): Double {(initial * pow(percent * 0.01 + 1, 3.0))
+fun accountInThreeYears(initial: Int, percent: Int) {
+    (initial * pow(percent * 0.01 + 1, 3.0))
 }
 
 /**
@@ -135,4 +138,7 @@ fun accountInThreeYears(initial: Int, percent: Int): Double {(initial * pow(perc
  * Пользователь задает целое трехзначное число (например, 478).
  * Необходимо вывести число, полученное из заданного перестановкой цифр в обратном порядке (например, 874).
  */
-fun numberRevert(number: Int): Int {(number / 100 + number / 10 % 10 * 10 + number % 10 * 100)}
+fun numberRevert(number: Int) {
+    (number / 100 + number / 10 % 10 * 10 + number % 10 * 100)
+}
+
--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -1,4 +1,5 @@
 @file:Suppress("UNUSED_PARAMETER")
+
 package lesson2.task2
 
 import lesson1.task1.sqr
@@ -17,8 +18,8 @@ fun pointInsideCircle(x: Double, y: Double, x0: Double, y0: Double, r: Double) =
  * Четырехзначное число назовем счастливым, если сумма первых двух ее цифр равна сумме двух последних.
  * Определить, счастливое ли заданное число, вернуть true, если это так.
  */
-fun isNumberHappy(number: Int): Boolean {
-    ((number/1000+number/100%10)==(number%10+number/10%10))
+fun isNumberHappy(number: Int) {
+    ((number / 1000 + number / 100 % 10) == (number % 10 + number / 10 % 10))
 }
 
 /**
@@ -28,8 +29,8 @@ fun isNumberHappy(number: Int): Boolean {
  * Определить, угрожают ли они друг другу. Вернуть true, если угрожают.
  * Считать, что ферзи не могут загораживать друг друга.
  */
-fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean {
-    ((x1==x2)||(y1==y2)||(Math.abs(x1-x2)==Math.abs(y1-y2)))
+fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int) {
+    ((x1 == x2) || (y1 == y2) || (Math.abs(x1 - x2) == Math.abs(y1 - y2)))
 }
 
 
@@ -39,11 +40,18 @@ fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean {
  * Дан номер месяца (от 1 до 12 включительно) и год (положительный).
  * Вернуть число дней в этом месяце этого года по григорианскому календарю.
  */
-fun daysInMonth(month: Int, year: Int): Int{
-    1,3,5,7,8,10,12 -> 31
-    4,6,9,11 -> 30
-    else if ((yeaar % 4 == 0) && (year % 100 != 0) or (year % 400 == 0)) 29 else 28
-}
+fun daysInMonth(month: Int, year: Int): Int =
+
+        if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0) && month == 2) 29
+        else when (month) {
+
+            1, 3, 5, 7, 8, 10, 12 -> 31
+
+            2 -> 28
+
+            else -> 30
+
+        }
 
 /**
  * Средняя
@@ -64,7 +72,6 @@ fun circleInside(x1: Double, y1: Double, r1: Double,
  * кирпич 4 х 4 х 4 пройдёт через отверстие 4 х 4.
  * Вернуть true, если кирпич пройдёт
  */
-fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean {
-    val b1 = (a <= r && b <= s) || (b <= r && a <= s) || (a <= r && c <= s) || (a <= s && c <= r) || (b <= r && c <= s) || (c <= r && b <= s)
-    return b1
+fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int) {
+    (a <= r && b <= s) || (b <= r && a <= s) || (a <= r && c <= s) || (a <= s && c <= r) || (b <= r && c <= s) || (c <= r && b <= s)
 }
